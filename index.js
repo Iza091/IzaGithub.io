a1aa12851089ea878defd660d0ed9e7c62f34074
Eduardo Isaías Villegas Ortiz <44987358+Eddo091@users.noreply.github.com>
2022-03-18 08:22:42 -0600

    Nav, Back to top, Diseño  son ahora responsivos

--- index.js
+++ index.js
@@ -54,6 +54,8 @@ let alterStyles = (isBackToTopRendered) => {
     : "scale(0)";
 };
 
+
+
 window.addEventListener("scroll", () => {
   if (window.scrollY > 700) {
     isBackToTopRendered = true;
@@ -63,4 +65,44 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 
+}
+
+
+);
+
+
+$('document').ready(function() {
+  var nav_height = 70;
+  
+  $("a[data-role='smoothscroll']").click(function(e) {
+    e.preventDefault();
+    
+    var position = $($(this).attr("href")).offset().top - nav_height;
+    
+    $("body, html").animate({
+      scrollTop: position
+    }, 1000 );
+    return false;
+  });
 });
+/*
+$('document').ready(function() {
+  // Back to top
+  var backTop = $(".back-to-top");
+  
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 400) {
+      backTop.css('visibility', 'visible');
+    }
+    else if($(document).scrollTop() < 400) {
+      backTop.css('visibility', 'hidden');
+    }
+  });
+  
+  backTop.click(function() {
+    $('html').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});*/
