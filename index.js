01c01ad4b2bcd9146335ccec6abebc6d7a2af986
Eduardo Isaías Villegas Ortiz <44987358+Eddo091@users.noreply.github.com>
2022-03-19 19:36:19 -0600

    BackTotop, funcional

--- index.js
+++ index.js
@@ -69,4 +69,28 @@ window.addEventListener("scroll", () => {
 
 
 );
+//Llamar al botón
+let mybutton = document.getElementById("btn-back-to-top");
 
+// Cuando se baja un 20% aparece
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// Al darle click, sube hasta arriba
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
